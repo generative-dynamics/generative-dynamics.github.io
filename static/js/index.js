ebdd7c050b875494da0702e84dd9cc7f6a3efae4
Aleksander Hołyński <alik.holynski@gmail.com>
2023-09-14 15:29:43 -0700

    Add autoplay to carousel, make it 5s

--- static/js/index.js
+++ static/js/index.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 			infinite: true,
 			pagination: false,
 			autoplay: true,
-			autoplaySpeed: 3000,
+			autoplaySpeed: 5000,
     }
 
 		// Initialize all div with carousel class
